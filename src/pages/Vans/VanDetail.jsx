042383e8d045bc45648c09dc64b81f33dbb791fc
Stella <stellaloh16@gmail.com>
2023-08-02 22:52:02 +0900

    Parallelized data loaders for VanDetail

--- src/pages/Vans/VanDetail.jsx
+++ src/pages/Vans/VanDetail.jsx
@@ -1,16 +1,44 @@
-import { Link, useLocation, useLoaderData } from "react-router-dom";
+import { Suspense } from "react";
+import {
+  Link,
+  useLocation,
+  useLoaderData,
+  defer,
+  Await,
+} from "react-router-dom";
 import { getVans } from "../../api";
 
 export function loader({ params }) {
-  return getVans(params.id);
+  return defer({ van: getVans(params.id) });
 }
 
 const VanDetail = () => {
   const location = useLocation();
-  const van = useLoaderData();
+  const dataPromise = useLoaderData();
 
   const search = location.state?.search || "";
   const type = location.state?.type || "all";
+
+  const renderVanDetail = (van) => {
+    return (
+      <>
+        {van ? (
+          <div className="van-detail">
+            <img src={van.imageUrl} />
+            <i className={`van-type ${van.type} selected`}>{van.type}</i>
+            <h2>{van.name}</h2>
+            <p className="van-price">
+              <span>${van.price}</span>/day
+            </p>
+            <p>{van.description}</p>
+            <button className="link-button">Rent this van</button>
+          </div>
+        ) : (
+          <h2>Loading...</h2>
+        )}
+      </>
+    );
+  };
 
   return (
     <div className="van-detail-container">
@@ -18,20 +46,9 @@ const VanDetail = () => {
         &larr;
         <span>Back to {type} vans</span>
       </Link>
-      {van ? (
-        <div className="van-detail">
-          <img src={van.imageUrl} />
-          <i className={`van-type ${van.type} selected`}>{van.type}</i>
-          <h2>{van.name}</h2>
-          <p className="van-price">
-            <span>${van.price}</span>/day
-          </p>
-          <p>{van.description}</p>
-          <button className="link-button">Rent this van</button>
-        </div>
-      ) : (
-        <h2>Loading...</h2>
-      )}
+      <Suspense fallback={<h2>Loading a van...</h2>}>
+        <Await resolve={dataPromise.van}>{renderVanDetail}</Await>
+      </Suspense>
     </div>
   );
 };
