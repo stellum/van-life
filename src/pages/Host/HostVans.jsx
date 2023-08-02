d6ce1dcd2f1f325379e63eb57a8d3cc90b579e4b
Stella <stellaloh16@gmail.com>
2023-08-02 22:43:37 +0900

    Updated data loaders for HostVans

--- src/pages/Host/HostVans.jsx
+++ src/pages/Host/HostVans.jsx
@@ -4,38 +4,38 @@ import { getHostVans } from "../../api";
 import { requireAuth } from "../../utils";
 
 export async function loader({ request }) {
-  requireAuth(request);
-  return defer({ hostVans: getHostVans() });
+  await requireAuth(request);
+  return defer({ vans: getHostVans() });
 }
 
 const HostVans = () => {
   const dataPromise = useLoaderData();
 
+  const renderVanElements = (vans) => {
+    const hostVanEls = vans.map((van) => (
+      <Link to={van.id} key={van.id} className="host-van-link-wrapper">
+        <div className="host-van-single" key={van.id}>
+          <img src={van.imageUrl} alt={`Photo of ${van.name}`} />
+          <div className="host-van-info">
+            <h3>{van.name}</h3>
+            <p>${van.price}/day</p>
+          </div>
+        </div>
+      </Link>
+    ));
+
+    return (
+      <div className="host-vans-list">
+        <section>{hostVanEls}</section>
+      </div>
+    );
+  };
+
   return (
     <section>
       <h1 className="host-vans-title">Your listed vans</h1>
       <Suspense fallback={<h2>Loading vans...</h2>}>
-        <Await resolve={dataPromise.hostVans}>
-          {(vans) => {
-            const hostVanEls = vans.map((van) => (
-              <Link to={van.id} key={van.id} className="host-van-link-wrapper">
-                <div className="host-van-single" key={van.id}>
-                  <img src={van.imageUrl} alt={`Photo of ${van.name}`} />
-                  <div className="host-van-info">
-                    <h3>{van.name}</h3>
-                    <p>${van.price}/day</p>
-                  </div>
-                </div>
-              </Link>
-            ));
-
-            return (
-              <div className="host-vans-list">
-                <section>{hostVanEls}</section>
-              </div>
-            );
-          }}
-        </Await>
+        <Await resolve={dataPromise.vans}>{renderVanElements}</Await>
       </Suspense>
     </section>
   );
