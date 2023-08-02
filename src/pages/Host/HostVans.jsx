60196aaf3e73b3f4e2e2281cfe800c2d551a5145
Stella <stellaloh16@gmail.com>
2023-08-02 22:25:41 +0900

    Parallelized data loaders for HostVans

--- src/pages/Host/HostVans.jsx
+++ src/pages/Host/HostVans.jsx
@@ -1,33 +1,42 @@
-import { Link, useLoaderData } from "react-router-dom";
+import { Suspense } from "react";
+import { Link, useLoaderData, defer, Await } from "react-router-dom";
 import { getHostVans } from "../../api";
 import { requireAuth } from "../../utils";
 
 export async function loader({ request }) {
-  await requireAuth(request);
-  return getHostVans();
+  requireAuth(request);
+  return defer({ hostVans: getHostVans() });
 }
 
 const HostVans = () => {
-  const vans = useLoaderData();
-
-  const hostVanEls = vans.map((van) => (
-    <Link to={van.id} key={van.id} className="host-van-link-wrapper">
-      <div className="host-van-single" key={van.id}>
-        <img src={van.imageUrl} alt={`Photo of ${van.name}`} />
-        <div className="host-van-info">
-          <h3>{van.name}</h3>
-          <p>${van.price}/day</p>
-        </div>
-      </div>
-    </Link>
-  ));
+  const dataPromise = useLoaderData();
 
   return (
     <section>
       <h1 className="host-vans-title">Your listed vans</h1>
-      <div className="host-vans-list">
-        <section>{hostVanEls}</section>
-      </div>
+      <Suspense fallback={<h2>Loading vans...</h2>}>
+        <Await resolve={dataPromise.hostVans}>
+          {(vans) => {
+            const hostVanEls = vans.map((van) => (
+              <Link to={van.id} key={van.id} className="host-van-link-wrapper">
+                <div className="host-van-single" key={van.id}>
+                  <img src={van.imageUrl} alt={`Photo of ${van.name}`} />
+                  <div className="host-van-info">
+                    <h3>{van.name}</h3>
+                    <p>${van.price}/day</p>
+                  </div>
+                </div>
+              </Link>
+            ));
+
+            return (
+              <div className="host-vans-list">
+                <section>{hostVanEls}</section>
+              </div>
+            );
+          }}
+        </Await>
+      </Suspense>
     </section>
   );
 };
