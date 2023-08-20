a5a9a2fae486bf505f91a994349ca6cbc584ac4b
Stella <stellaloh16@gmail.com>
2023-08-20 16:35:32 +0900

    Updated logout btn display upon loggedin state

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,16 @@
+import PropTypes from "prop-types";
 import { Link, NavLink } from "react-router-dom";
 import imageUrl from "../assets/images/avatar-icon.png";
 import logo from "../assets/images/logo.png";
 
-const Header = () => {
-  function fakeLogOut() {
-    localStorage.removeItem("loggedin");
-  }
+const Header = ({ isLoggedIn, onLogout }) => {
+  console.log("isLoggedIn prop:", isLoggedIn);
+
+  // function fakeLogOut() {
+  //   localStorage.removeItem("loggedin");
+  // }
+
+  // const isLoggedIn = localStorage.getItem("loggedin") === "true";
 
   return (
     <header>
@@ -32,13 +37,27 @@ const Header = () => {
         >
           Vans
         </NavLink>
-        <Link to="login" className="login-link">
-          <img src={imageUrl} className="login-icon" />
-        </Link>
-        <button onClick={fakeLogOut}>Log out</button>
+
+        {isLoggedIn ? (
+          <>
+            <Link to="host" className="login-link">
+              <img src={imageUrl} className="login-icon" />
+            </Link>
+            <button onClick={onLogout}>Log out</button>
+          </>
+        ) : (
+          <Link to="login" className="login-link">
+            <img src={imageUrl} className="login-icon" />
+          </Link>
+        )}
       </nav>
     </header>
   );
+};
+
+Header.propTypes = {
+  isLoggedIn: PropTypes.bool.isRequired,
+  onLogout: PropTypes.func.isRequired,
 };
 
 export default Header;
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,23 @@
 import { Outlet } from "react-router-dom";
+import { useState, useEffect } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 
 const Layout = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const storedLoginStatus = localStorage.getItem("loggedin");
+    setIsLoggedIn(storedLoginStatus === "true");
+  }, []);
+
+  function fakeLogOut() {
+    localStorage.removeItem("loggedin");
+  }
+
   return (
     <div className="site-wrapper">
-      <Header />
+      <Header isLoggedIn={isLoggedIn} onLogout={fakeLogOut} />
       <main>
         <Outlet />
       </main>
